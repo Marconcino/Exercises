17e06c7b10d2704c644124a034bf7cdb78cf9db3
MarioRacel <s.marioracel.rosin@progettomarconi.com>
2025-10-31 12:19:27 +0100

    Main e Crud Modificati

--- CRUD_WebServer/main.py
+++ CRUD_WebServer/main.py
@@ -1,1 +1,9 @@
-from fastapi import FastAPI
+from sqlalchemy import Column, Integer, String
+from database.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50))
+    email = Column(String(100), unique=True, index=True)
