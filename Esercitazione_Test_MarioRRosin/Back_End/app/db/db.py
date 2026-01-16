5a39d6ae7617ef395e9e6319d47b3b4749645529
MarioRacel <s.marioracel.rosin@progettomarconi.com>
2026-01-16 13:03:31 +0100

    Connecting with the db

--- Esercitazione_Test_MarioRRosin/Back_End/app/db/db.py
+++ Esercitazione_Test_MarioRRosin/Back_End/app/db/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # This has the task to connect to the database created on MySQL
 # Make sure to replace the user, password, host, port, and database name as needed
 DATABASE_URL = (
-    "postgresql+psycopg2://postgres:root@localhost:5432/medical_clinic_db"
+    "mysql+mysqlconnector://user_service_db:SQLsql!!!@localhost:3306/medical_clinic_db"
 )
 
 engine = create_engine(
