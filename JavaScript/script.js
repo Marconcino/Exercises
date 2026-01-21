20e992389a1d7cac0e7f3ed3796c4f49a5aa49cd
MarioRacel <s.marioracel.rosin@progettomarconi.com>
2026-01-21 15:12:15 +0100

    Alert Examples

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+alert("Hello, I'm JavaScript!"); // Pop Up del risulato all'interno delle parentesi
+alert("What's 1 + 1 + 1 + 1 + 1 + 1 ? = "); 
+alert(3 +
+1
++ 2); 
+// In quest'ultimo caso il pop up mostrerà 6 
+// e non ha importanza che i numeri siano scritti su righe diverse
+alert("Simpoh")
