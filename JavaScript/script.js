04491bf4e752b174ed994259d3878161272900e9
MarioRacel <s.marioracel.rosin@progettomarconi.com>
2026-01-22 10:00:17 +0100

    Script.js Updated

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,6 @@
+/* 
+// Primi esempi di JavaScript
+// Alert 
 alert("Hello, I'm JavaScript!"); // Pop Up del risulato all'interno delle parentesi
 alert("What's 1 + 1 + 1 + 1 + 1 + 1 ? = "); 
 alert(3 +
@@ -5,4 +8,21 @@ alert(3 +
 + 2); 
 // In quest'ultimo caso il pop up mostrerà 6 
 // e non ha importanza che i numeri siano scritti su righe diverse
-alert("Simpoh")
+alert("It's Simpoh")
+*/
+
+/*
+// Usiamo Strict Mode per evitare errori comuni, come ad esempio la prevenzione di bug
+// Utilizzato nei progetti moderni
+"use strict"; // Modalità Strict
+function test() {
+  console.log(this);
+}
+test();
+*/
+
+// Variabili
+let message;
+message = 'Hello';
+alert(message); // Mostra Hello
+
