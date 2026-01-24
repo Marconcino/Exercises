a19f50843c40f203a94d03dd51c795de99b40de7
MarioRacel <s.marioracel.rosin@progettomarconi.com>
2026-01-25 00:46:56 +0100

    Ended Esercizi 1

--- Esercizi_JS/Esercizi 1.js
+++ Esercizi_JS/Esercizi 1.js
@@ -3,14 +3,15 @@
 
 // 1. Dichiara una variabile nome usando let e assegnale una stringa.
 let name = "This is a string";
+console.log(name);
 
 // 2. Dichiara una variabile eta usando const.
 const eta = 28;
+console.log(eta);
 
 // 3. Prova a riassegnare eta. Cosa succede?
 age = eta;
 console.log(age);
-
 
 // 4. Dichiara due numeri a e b e stampa somma, differenza, prodotto e divisione.
 let a = 1;
@@ -23,7 +24,7 @@ console.log(a / b);
 
 // 5. Stampa il tipo (typeof) di una stringa, un numero e un booleano.
 console.log(typeof(name));
-console.log(typeof(a));
+console.log(typeof(age));
 boolean = true;
 console.log(typeof(boolean));
 
@@ -36,10 +37,12 @@ console.log(x); // Si è permesso
 
 
 // 7. Stampa il tipo di x prima e dopo la riassegnazione.
-
+console.log(typeof(x));
+console.log(typeof(x));
 
 // 8. Dichiara una variabile senza assegnarle un valore e stampane il tipo.
-
+let y = "";
+console.log(x);
 
 // 9. Confronta il valore null e undefined usando typeof.
 console.log(typeof(null));
@@ -47,12 +50,19 @@ console.log(typeof(undefined));
 
 // 10. Somma "10" + 5. Cosa ottieni?
 console.log("10" + 5);
+// Si ottiene 105, 
+// in quanto JS somma il contenuto della striga 
+// SENZA sommare il contenuto di un integer
 
 // 11. Somma "10" - 5. Cosa ottieni?
 console.log("10" - 5);
+// In questo caso si ottiene 5
+// JS toglie completamente il valore della stringa
 
-// 12. Moltiplica "10" \* 2.
+// 12. Moltiplica "10" * 2.
 console.log("10" * 2);
+// Il risultato è 20
+ 
 
 // 13. Dividi "20" / "2".
 console.log("20" / "2")
@@ -68,7 +78,10 @@ console.log("5" == 5)
 console.log("5" === 5)
 
 // 17. Spiega la differenza tra == e === usando un commento.
-// La differenza tra == e === è che la prima dà 
+// La differenza tra == e === è che:
+// la prima verifica se il valore all'interno della stringa corrisponde a quello del numero intero, 
+// quindi il risultato è vero;
+// mentre, la seconda, risulta falso, in quanto si verifica se la stringa equivale ad una stringa; 
 
 
 // 18. Confronta null == undefined.
@@ -77,4 +90,17 @@ console.log(null == undefined); // True
 // 19. Confronta null === undefined.
 console.log(null === undefined); // False
 
-// 20. Usa gli operatori +=, -=, \*=, /= su una variabile numerica.
+// 20. Usa gli operatori di assegnazione composta +=, -=, \*=, /= su una variabile numerica. 
+let number = 5;
+
+number += 1;
+console.log(number);
+
+number -= 2;
+console.log(number); // Questo viene sottratto dell'ultimo valore stampato
+
+number *= 3;
+console.log(number); // Stessa cosa capita qui
+
+number /= 4;
+console.log(number); // E qui
